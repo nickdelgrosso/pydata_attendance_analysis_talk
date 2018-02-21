ed69dda832d1297e533170a6dc5aa9f21ecdc97c
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2018-02-21 19:47:30 +0100

    put into pandas

--- pydata_attendance.py
+++ pydata_attendance.py
@@ -1,4 +1,6 @@
 import requests
+import pandas as pd
+
 
 url = 'https://api.meetup.com/PyData-Munchen/events?&status=past'
 r = requests.get(url)
@@ -6,3 +8,7 @@ r = requests.get(url)
 print(r.status_code)
 
 print(r.json())
+
+# Put into a Pandas dataframe
+df = pd.DataFrame(r.json())
+print(df)
